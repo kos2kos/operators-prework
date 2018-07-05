a8f3ae1c3dcc7b3b03f219776db484a5ce9a5e3c
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-05 17:33:21 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 40 && speed < 60
+    FALSE
+  elsif speed < 40
+    TRUE
+  elsif speed >60
+    TRUE
 end
 
 
 
 def not_safe?(speed)
-	
+	!(unsafe(speed))
 end
 	
 
